a823dff5fa892890b4af1e521238848a7d4ae4fc
Christopher Edelmaier <christopher.edelmaier@gmail.com>
2015-02-17 12:35:49 -0700

    D stuff

--- dPrimes/dPrimes.d
+++ dPrimes/dPrimes.d
@@ -14,10 +14,19 @@ void main(string[] args) {
     sw.start();
     auto e = eratosthenes(limit);
     sw.stop();
-    auto elapsed = sw.peek().usecs / 1_000.0;
-    printf("Eratosthenes: %.4fms\n", elapsed);
+    auto elapsedE = sw.peek().usecs / 1_000.0;
+    printf("Eratosthenes: %.4fms\n", elapsedE);
     printf("\tPrimes counted: %ld\n", e[0]);
     printf("\tMax prime: %ld\n\n\n", e[1]);
+
+    sw.reset();
+    sw.start();
+    auto ip2 = iprimes2(limit);
+    sw.stop();
+    auto elapsedIp2 = sw.peek().usecs / 1_000.0;
+    printf("Iprimes2: %.4fms\n", elapsedIp2);
+    printf("\tPrimes counted: %ld\n", ip2[0]);
+    printf("\tMax prime: %ld\n\n\n", ip2[1]);
 }
 
 //Typecons gets us tuple operations, neat huh
@@ -43,6 +52,32 @@ Tuple!(uint, uint) eratosthenes(uint limit) {
     }
 
     pc -= 2;
+
+    return tuple(pc, maxprime);
+}
+
+Tuple!(uint, uint) iprimes2(uint limit) {
+    uint lmtbf = (limit - 3) / 2;
+    bool[] buf = new bool[lmtbf+1];
+    buf[] = true;
+
+    for(uint i = 0; i < (sqrt(cast(float)limit) - 3) / 2 + 1; i++) {
+        if(buf[i]) {
+            uint p = i + i + 3;
+            uint s = p * (i + 1) + i;
+            for(uint j = s; j < (lmtbf + 1); j += p) {
+                buf[j] = false;
+            }
+        }
+    }
+
+    uint pc = 1, maxprime = 0;
+    for(uint i = 0; i < (lmtbf + 1); i++) {
+        if(buf[i]) {
+            pc++;
+            maxprime = i + i + 3;
+        }
+    }
 
     return tuple(pc, maxprime);
 }
