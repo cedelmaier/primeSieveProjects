dd8e35f36f31520ea185235f699819707b35b656
Chris Edelmaier <chrise@spectralogic.com>
2015-06-30 14:57:27 -0600

    Added timing functions to rust main functions

--- rust_primes/src/main.rs
+++ rust_primes/src/main.rs
@@ -1,5 +1,6 @@
 /// Eratosthenes prime sieve program
 extern crate rust_primes;
+extern crate time;
 
 use rust_primes::eratosthenes;
 use rust_primes::primes2;
@@ -17,21 +18,27 @@ fn main() {
     let limit = usize::pow(2, input_num);
     println!("Limit: {}", limit);
 
+    let mut start = time::precise_time_ns();
     let (era_pc, era_max) = eratosthenes(limit);
+    let mut end = time::precise_time_ns();
 
-    println!("Eratosthenes");
+    println!("Eratosthenes: {}ms", (end - start)/1_000_000);
     println!("\tPrimes Counted: {}", era_pc);
     println!("\tMaxPrime: {}", era_max);
 
+    start = time::precise_time_ns();
     let (p2_pc, p2_max) = primes2(limit);
+    end = time::precise_time_ns();
 
-    println!("Primes2(odd)");
+    println!("Primes2(odd): {}ms", (end - start)/1_000_000);
     println!("\tPrimes Counted: {}", p2_pc);
     println!("\tMaxPrime: {}", p2_max);
 
+    start = time::precise_time_ns();
     let (p235_pc, p235_max) = primes235(limit);
+    end = time::precise_time_ns();
 
-    println!("Primes235");
+    println!("Primes235 {}ms", (end - start)/1_000_000);
     println!("\tPrimes Counted: {}", p235_pc);
     println!("\tMaxPrime: {}", p235_max);
 }
